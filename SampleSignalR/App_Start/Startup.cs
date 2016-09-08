bf9b3d95908538d480db8ff6ca5e10a54f830ea1
meltay <meltay.isilti@ogr.deu.edu.tr>
2016-09-08 22:27:34 +0300

    Sample SignalR

---
+++ SampleSignalR/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SampleSignalR.App_Start.Startup))]
+
+namespace SampleSignalR.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(); 
+            
+        }
+    }
+}
---
+++ SampleSignalR/Hub/UserHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SampleSignalR
+{
+    [HubName("UserHub")]
+    public class UserHub : Hub
+    {
+        [HubMethodName("GetUser")]
+        public void GetUser(string userName, int userAge, int userSalary)
+        {
+            Clients.All.addUser(userName,userAge,userSalary);
+        }
+    }
+}
